a583b60bfd5268f72202eeb24b6ff796e89075dc
Akshit Kallepalli <akshit.kallepalli@gmail.com>
2023-10-15 21:07:47 -0400

    adding

--- healthz.test.js
+++ healthz.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app'); // make sure the path is correct to your app.js or server.js
+const app = require('./app'); // make sure the path is correct to your app.js or server.js
 
 describe('GET /healthz', () => {
   it('responds with json', async () => {
