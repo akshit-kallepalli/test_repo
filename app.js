6cc8f7c194a04f05f50038dc19f1469e4bc87b6a
Akshit Kallepalli <akshit.kallepalli@gmail.com>
2023-10-15 23:25:43 -0400

    adding

--- app.js
+++ app.js
@@ -222,8 +222,10 @@ app.patch('/v1/assignments/:id', basicAuth, async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
   });
+}
 
-  module.exports = app;
+module.exports = app;
